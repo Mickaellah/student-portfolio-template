9df958f1abe029c61af14b7977b89d843e1a8552
Nomenjanahary Clopedia <clopedia.nom@onja.org>
2020-11-28 09:44:19 +0300

    Moved everything to index.html, used index.html for a start, created webroot folder and moved everything there. Deleted all javascript files.

--- dist/student-portfolio-template.e31bb0bc.js
+++ dist/student-portfolio-template.e31bb0bc.js
@@ -29772,17 +29772,73 @@ if ("development" === 'production') {
 } else {
   module.exports = require('./cjs/react-dom.development.js');
 }
-},{"./cjs/react-dom.development.js":"node_modules/react-dom/cjs/react-dom.development.js"}],"index.js":[function(require,module,exports) {
+},{"./cjs/react-dom.development.js":"node_modules/react-dom/cjs/react-dom.development.js"}],"assets/hary.jpg":[function(require,module,exports) {
+module.exports = "/hary.dc393f7e.jpg";
+},{}],"components/About.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _hary = _interopRequireDefault(require("../assets/hary.jpg"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function About() {
+  return /*#__PURE__*/_react.default.createElement("article", {
+    className: "about_cart"
+  }, /*#__PURE__*/_react.default.createElement("div", {
+    className: "about_cart__about_me"
+  }, /*#__PURE__*/_react.default.createElement("img", {
+    src: _hary.default,
+    alt: "My profile"
+  }), /*#__PURE__*/_react.default.createElement("h2", null, "Clopedia Nomenjanahary"), /*#__PURE__*/_react.default.createElement("p", null, "Front-end developer")), /*#__PURE__*/_react.default.createElement("div", {
+    className: "contact"
+  }, /*#__PURE__*/_react.default.createElement("a", {
+    className: "contact__email",
+    href: "mailto:nomenjanahary.clopedia@gmail.com"
+  }, "nomenjanahary.clopedia@gmail.com")), /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("p", null, "My name is Clopedia, I've started to learn coding for a year. Before, I didn't have any idea on how to write a piece of code, I used a piece of software without understanding the functionality and the component that used to build it.")));
+}
+
+var _default = About;
+exports.default = _default;
+},{"react":"node_modules/react/index.js","../assets/hary.jpg":"assets/hary.jpg"}],"App.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _About = _interopRequireDefault(require("./components/About"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function App() {
+  return /*#__PURE__*/_react.default.createElement(_react.default.Fragment, null, /*#__PURE__*/_react.default.createElement(_About.default, null));
+}
+
+var _default = App;
+exports.default = _default;
+},{"react":"node_modules/react/index.js","./components/About":"components/About.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
 
 var _reactDom = _interopRequireDefault(require("react-dom"));
 
+var _App = _interopRequireDefault(require("./App"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_reactDom.default.render( /*#__PURE__*/_react.default.createElement("h1", null, "Hello world!!!"), document.getElementById('root'));
-},{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_App.default, null), document.getElementById('root'));
+},{"react":"node_modules/react/index.js","react-dom":"node_modules/react-dom/index.js","./App":"App.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -29810,7 +29866,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61164" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61369" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- dist/styles.8986bff4.js
+++ dist/styles.8986bff4.js
@@ -184,7 +184,12 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./assets\\email-24px.svg":[["email-24px.949ef630.svg","assets/email-24px.svg"],"assets/email-24px.svg"],"_css_loader":"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -212,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61164" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61369" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
